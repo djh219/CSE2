6034f062c9275d0e601df87aeb331b43ab7bea8b
djh219 <djh219@lehigh.edu>
2015-08-31 18:50:51 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,33 @@
+//Derek Huber
+//Homework 1
+//Welcome Class Java Program
+//
+//  first, compile the program
+//      javac WelcomeClass.java
+//  then, run the program
+//      java WelcomeClass
+
+//  define the class
+public class WelcomeClass {
+    
+    //add the main method
+    public static void main(String[] args)  {
+    
+        //print the first three lines, exactly as in the assignment
+        System.out.println("  -----------");
+        System.out.println("  | WELCOME |");
+        System.out.println("  -----------");
+
+        //Now, print the series of symbols and the student ID
+        System.out.println("  ^  ^  ^  ^  ^  ^  ");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+        System.out.println("|-D--J--H--2--1--9-|");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+        System.out.println("  v  v  v  v  v  v  ");
+        
+        //Next, print the tweet length autobiographic statemtent
+        System.out.println("I am studying computer science and business at Lehigh University, and my favorite color is blue.");
+        
+        
+    }
+}
